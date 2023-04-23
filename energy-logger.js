3c338e6f4a928b5e63be2b99799afb5df2b0004b
Antony Vasiliev <tony.vasiliev8@gmail.com>
2023-04-22 23:03:10 -0400

    Added janky energy logger

--- energy-logger.js
+++ energy-logger.js
@@ -1,17 +1,63 @@
 const fs = require('fs');
 const EnergyMonitor = require('./energy-monitor');
 
+const DUMP_LOG_PATH = `${__dirname}/dump`
+
 class EnergyLogger{
     constructor(configPath) {
         this.configPath = configPath;
+        this.plugs = {};
     }
 
     start() {
         return new Promise((resolve, reject) => {
             this.readConfig().then(config => {
-                resolve(config)
+                for (let plug in config){
+                    let name = config[plug].name;
+                    let address = config[plug].address;
+                    let interval = config[plug].poll_interval;
+
+                    this.plugs[name] = new EnergyMonitor(name, address, interval);
+                    this.plugs[name].start(data => {
+                        this.onData(plug, data);
+                    }, error => {
+                        this.onError(plug, error);
+                    })
+                }
+
+                resolve();
             }).catch(reject);
         })
+    }
+
+    onData(plug, data){
+        try{
+            let date = new Date();
+            let year = date.getFullYear();
+            let month = String(date.getMonth()).padStart(2, '0');
+            let day = String(date.getDay()).padStart(2, '0');
+
+            let name = `${plug}_${year}-${month}-${day}`;
+            let path = `${DUMP_LOG_PATH}/${name}.csv`
+
+            let line = [`Time:${date.getTime()}`];
+            for (let entry of Object.entries(data)){
+                line.push(entry.join(':'))
+            }
+
+            fs.appendFile(path, line.join(',') + '\n', err => {
+                if (err){
+                    console.error(err);
+                }
+            })
+        }
+        catch(err){
+            console.error(plug, err);
+        }
+    }
+
+    onError(plug, error){
+        console.error(plug, error);
     }
 
     readConfig(){
--- energy-monitor.js
+++ energy-monitor.js
@@ -1,5 +1,53 @@
-class EnergyMonitor{
+const http = require('http');
 
+const HTTP_URI = 'cm?cmnd=status%2010';
+const ENERGY_RESPONSE_KEYS = ['Voltage', 'Current', 'Factor', 'Power', 'ApparentPower', 'ReactivePower']
+
+class EnergyMonitor{
+    constructor(name, address, interval){
+        this.name = name;
+        this.address = address;
+        this.interval = interval;
+    }
+
+    start(onData, onError){
+        this.handle = setInterval(() => {
+            this.fetch().then(onData).catch(onError);
+        }, this.interval);
+    }
+
+    fetch(){
+        return new Promise((resolve, reject) => {
+            let url = `http://${this.address}/${HTTP_URI}`
+
+            http.get(url, res => {
+                if (res.statusCode == 200){
+                    let data = [];
+                    
+                    res.on('data', d => {
+                        data.push(d);
+                    })
+
+                    res.on('end', () => {
+                        let json = JSON.parse(Buffer.concat(data).toString())
+                        let energy = json.StatusSNS.ENERGY;
+                        let values = {};
+
+                        for (let key of ENERGY_RESPONSE_KEYS){
+                            values[key] = energy[key];
+                        }
+
+                        resolve(values);
+                    })
+                }
+                else{
+                    reject(`${this.name}: ${res.statusCode}`);
+                }
+            }).on('error', err => {
+                reject(err);
+            });
+        })
+    }
 }
 
 module.exports = EnergyMonitor
