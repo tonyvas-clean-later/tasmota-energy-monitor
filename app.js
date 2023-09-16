0e512496a02e0c99c577c69cf5c65e8bdceba737
Antony Vasiliev <tony.vasiliev8@gmail.com>
2023-09-15 22:17:28 -0400

    Implemented scraper

--- app.js
+++ app.js
@@ -2,6 +2,15 @@ process.env.DATABASE_DIRPATH = `${__dirname}/db`
 process.env.SCRAPER_DIRPATH = `${__dirname}/scraper`
 
 const Scraper = require(`${process.env.SCRAPER_DIRPATH}/scraper.js`);
+const DatabaseManager = require(`${process.env.DATABASE_DIRPATH}/database-manager.js`)
 
 let scraper = new Scraper();
-scraper.runbs();
+scraper.start();
+
+let db = new DatabaseManager();
+setInterval(async () => {
+    for (let name of ['S31 - UPS', 'S31 - NETMOX', 'S31 - MAINMOX', 'S31 - TRUENAS']){
+        let polls = await db.selectPollsByPlugName(name);
+        // console.log(name, polls.length);
+    }
+}, 5000);
--- scraper/scraper.js
+++ scraper/scraper.js
@@ -1,29 +1,124 @@
+const puppeteer = require('puppeteer');
+const cheerio = require('cheerio');
+
 const DatabaseManager = require(`${process.env.DATABASE_DIRPATH}/database-manager.js`)
+
+const ERROR_PREFIX = 'Scraper'
+const SCRAPE_INTERVAL_MS = 1000;
+
+const URL = "http://homeassistant.pve-2.home.lan:9541/devices";
+const VIEWPORT = {width: 1080, height: 1024};
 
 class Scraper{
     constructor() {
         this.db = new DatabaseManager();
     }
 
-    async runbs(){
-        let bs = [
-            {a: 'a', 'b': 1, 'c': 2, 'd': 3},
-            {a: 'a', 'b': 10, 'c': 20, 'd': 30},
-            {a: 'a', 'b': 100, 'c': 200, 'd': 300},
-            {a: 'b', 'b': 7, 'c': 8, 'd': 9},
-            {a: 'b', 'b': 77, 'c': 88, 'd': 99},
-            {a: 'b', 'b': 777, 'c': 888, 'd': 999},
-        ]
+    async start(){
+        try {
+            let browser = await puppeteer.launch({args: ['--no-sandbox', '--disable-setuid-sandbox']});
+            let page = await browser.newPage();
 
-        for (let s of bs){
-            await this.db.addPoll(s.a, s.b, s.c, s.d)
+            await page.goto(URL);
+            await page.setViewport(VIEWPORT);
+
+            setInterval(() => {
+                this.run(page).catch(console.error)
+            }, 1000);
+        } catch (error) {
+            throw new Error(`${ERROR_PREFIX}.start: failed to navigate website \n\t${error.message}`)
+        }
+    }
+
+    async run(page){
+        try {
+            let timestampMS = new Date().getTime();
+            let html = await page.content();
+            let plugs = this.scrape(html);
+            
+            for (let plug of plugs){
+                try {
+                    await this.db.addPoll(plug['name'], timestampMS, plug['runtime'], plug['energy']);
+                } catch (error) {
+                    console.error(error);
+                }
+            }
+        } catch (error) {
+            throw new Error(`${ERROR_PREFIX}.run: failed to get plugs \n\t${error.message}`)
+        }
+    }
+
+    scrape(html){
+        try {
+            let plugs = [];
+            let $ = cheerio.load(html);
+
+            for (let row of $('#device-list').find('tbody').find('tr')){
+                plugs.push(this.getPlugDataFromRow($, row));
+            }
+
+            return plugs;
+        } catch (error) {
+            throw new Error(`${ERROR_PREFIX}.scrape: failed to scrape website \n\t${error.message}`)
+        }
+    }
+
+    getPlugDataFromRow($, row){
+        try {
+            let plug = {};
+
+            for (let cell of $(row).find('td')){
+                let key = $(cell).find('b').text().trim();
+
+                switch (key) {
+                    case 'Name':
+                        plug['name'] = this.parseName($, cell);
+                        break;
+                    case 'Runtime':
+                        plug['runtime'] = this.parseRuntime($, cell);
+                        break;
+                    case 'Energy':
+                        plug['energy'] = this.parseEnergy($, cell);
+                        break;
+                }
+            }
+
+            return plug;
+        } catch (error) {
+            throw new Error(`${ERROR_PREFIX}.getPlugDataFromRow: failed to get plug data \n\t${error.message}`)
+        }
+    }
+
+    parseName($, cell){
+        let name = $(cell).find('a').text().trim();
+
+        // Remove evil non-breaking space
+        return name.replace(new RegExp(String.fromCharCode(160), 'g'), ' ');
+    }
+
+    parseRuntime($, cell){
+        const MULTIPLIERS = {
+            's': 1,
+            'm': 60,
+            'h': 60 * 60,
+            'd': 24 * 60 * 60
         }
 
-        let a = await this.db.selectPollsByPlugName('a');
-        let b = await this.db.selectPollsByPlugName('b');
+        let runtimeStr = $(cell).find('span').text().trim();
+        let runtimeS = 0;
 
-        console.log(a);
-        console.log(b);
+        for (let part of runtimeStr.split(' ')){
+            let value = part.substring(0, part.length - 1);
+            let key = part.substring(part.length - 1)
+
+            runtimeS += value * MULTIPLIERS[key];
+        }
+
+        return runtimeS
+    }
+
+    parseEnergy($, cell){
+        return Number($(cell).find('span').text().trim().split(' ')[0])
     }
 }
 
